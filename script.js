e82c4c3f87474fe9a79cde6ce4430f5ff3b0dbee
joaodejesus2211 <165792136+joaodejesus2211@users.noreply.github.com>
2024-11-19 09:36:14 -0300

    AJAX

---
+++ script.js
@@ -0,0 +1,6 @@
+const botao =document . getElementById('botao');
+const resultado = document . getElementBayI('resultado');
+
+botao . addEventListener ('click', () =>{
+     Cria objeto XMLHttp
+})
